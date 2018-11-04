d99c3948cde50748d21dd92e382f0803a9633984
unknown <judit.pacsai@gmail.com>
2018-11-04 21:50:03 +0100

    convert xml response to json

--- src/components/BookManager.vue
+++ src/components/BookManager.vue
@@ -68,16 +68,71 @@ export default {
   },
   methods: {
     searchBooks () {
-      fetch('https://cors-escape.herokuapp.com/https://www.goodreads.com/search/index.xml?key=' + keys.bookKey + '&q=Ender%27s+Game').then(data => data.blob())
+      fetch(
+        'https://cors-escape.herokuapp.com/https://www.goodreads.com/search/index.xml?key=' +
+          keys.bookKey +
+          '&q=Ender%27s+Game'
+      )
+        .then(data => data.blob())
         .then(data => {
           console.log(data)
           const reader = new FileReader()
           reader.onload = function (e) {
             const text = reader.result
-            return text
+            console.log(text)
+            const xml = text
+            const xmlDOM = new DOMParser().parseFromString(xml, 'text/xml')
+            const t = xmlToJson(xmlDOM)
+            function xmlToJson (xml) {
+              // Create the return object
+              let obj = {}
+
+              if (xml.nodeType === 1) {
+                // element
+                // do attributes
+                if (xml.attributes.length > 0) {
+                  obj['@attributes'] = {}
+                  for (let j = 0; j < xml.attributes.length; j += 1) {
+                    const attribute = xml.attributes.item(j)
+                    obj['@attributes'][attribute.nodeName] =
+                      attribute.nodeValue
+                  }
+                }
+              } else if (xml.nodeType === 3) {
+                // text
+                obj = xml.nodeValue
+              }
+
+              // do children
+              // If just one text node inside
+              if (
+                xml.hasChildNodes() &&
+                xml.childNodes.length === 1 &&
+                xml.childNodes[0].nodeType === 3
+              ) {
+                obj = xml.childNodes[0].nodeValue
+              } else if (xml.hasChildNodes()) {
+                for (let i = 0; i < xml.childNodes.length; i += 1) {
+                  const item = xml.childNodes.item(i)
+                  const nodeName = item.nodeName
+                  if (typeof obj[nodeName] === 'undefined') {
+                    obj[nodeName] = xmlToJson(item)
+                  } else {
+                    if (typeof obj[nodeName].push === 'undefined') {
+                      const old = obj[nodeName]
+                      obj[nodeName] = []
+                      obj[nodeName].push(old)
+                    }
+                    obj[nodeName].push(xmlToJson(item))
+                  }
+                }
+              }
+              return obj
+            }
           }
           reader.readAsText(data)
-        }).catch(function (error) {
+        })
+        .catch(function (error) {
           console.log('Looks like there was a problem: \n', error)
         })
     },
@@ -113,10 +168,10 @@ export default {
 </script>
 
 <style scoped>
-  .search-results {
-    width: 100%;
-    min-height: 200px;
-    margin: 10px 0;
-    border: 1px solid #DFDFDF;
-  }
+.search-results {
+  width: 100%;
+  min-height: 200px;
+  margin: 10px 0;
+  border: 1px solid #dfdfdf;
+}
 </style>
