3a0ffa7cd9f2be2600499bf1cf516cb71e144732
unknown <judit.pacsai@gmail.com>
2018-11-24 00:16:00 +0100

    feat: svg stars for rating

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -7,7 +7,22 @@
       <div class='info'>
         <p class='title'>{{ this.title }}</p>
         <p v-if="view === 'search'" class="author">by <span class='author-link' @click='authorBooks'>{{ this.author }}</span> - {{ this.year > 0 ? this.year : 'unknown' }}</p>
-        <p class="ratings">{{ this.rating }} avg rating</p>
+        <div class="ratings">
+          <p>{{ this.rating }} avg rating</p>
+          <svg width="100px" height="25px" viewBox="0 0 100 25">
+            <defs>
+              <linearGradient id="star" x1="0" y1="0" x2="1" y2="0">
+                <stop id="stop1" offset="50%" stop-color="purple"/>
+                <stop id="stop2" offset="50%" stop-color="lightgrey" />
+              </linearGradient>
+            </defs>
+            <path d="M9.29 1.1 L3.3 21.78 L19.8 8.58 L0 8.58 L16.5 21.78
+                     M28.29 1.1 L22.3 21.78 L38.8 8.58 L19 8.58 L35.5 21.78
+                     M47.29 1.1 L41.3 21.78 L57.8 8.58 L38 8.58 L54.5 21.78
+                     M66.29 1.1 L60.3 21.78 L76.8 8.58 L57 8.58 L73.5 21.78
+                     M85.29 1.1 L79.3 21.78 L95.8 8.58 L76 8.58 L92.5 21.78" fill="url(#star)" stroke="transparent" />
+          </svg>
+        </div> 
       </div>
       <div class="details">
         <button v-if="this.series === true && view !== 'series'" class="series-btn" @click='searchSeries'> Series</button>
