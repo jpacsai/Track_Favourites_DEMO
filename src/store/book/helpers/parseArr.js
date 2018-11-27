83d024f154cd72fb3cc39e2a78066d1c6649624e
unknown <judit.pacsai@gmail.com>
2018-11-28 00:15:44 +0100

    refactor: remove nested objects from book object

--- src/store/book/helpers/parseArr.js
+++ src/store/book/helpers/parseArr.js
@@ -15,18 +15,14 @@ export default function parseArr (arr, today) {
       imgUrl: obj.best_book ? obj.best_book.small_image_url || obj.best_book.image_url : obj.image_url,
       rating: obj.average_rating || +((obj.ratings_sum / obj.ratings_count).toFixed(2)) || 0,
       url: obj.link || 'https://www.goodreads.com/book/show/' + obj.best_book.id,
-      release: {
-        displayYear: obj.original_publication_year ? String(obj.original_publication_year) : obj.publication_year ? String(obj.publication_year) : 'unknown',
-        string: parseHelpers.releaseString(year, month, day),
-        dateObj: parseHelpers.releaseDate(year, month, day)
-      },
-      get future () { return this.release.dateObj > today },
-      serie: {
-        serie: titleDecode.includes('#'),
-        get title_without_serie () { return this.serie === true ? parseHelpers.noSeriesTitle(titleDecode) : null },
-        get serieTitle () { return this.serie === true ? parseHelpers.serieTitle(titleDecode) : null },
-        get position () { return this.serie === true ? obj.position : null }
-      }
+      displayYear: obj.original_publication_year ? String(obj.original_publication_year) : obj.publication_year ? String(obj.publication_year) : 'unknown',
+      displayDateString: parseHelpers.releaseString(year, month, day),
+      dateObj: parseHelpers.releaseDate(year, month, day),
+      get future () { return this.dateObj > today },
+      serie: titleDecode.includes('#'),
+      get title_without_serie () { return this.serie === true ? parseHelpers.noSeriesTitle(titleDecode) : null },
+      get serieTitle () { return this.serie === true ? parseHelpers.serieTitle(titleDecode) : null },
+      get position () { return this.serie === true ? obj.position : null }
     }
     return book
   })
