ead344704e1fa7239e5181ab011afa9ce3baff37
unknown <judit.pacsai@gmail.com>
2018-12-01 23:45:14 +0100

    feat: create reminder store

---
+++ src/store/reminder/store_reminders.js
@@ -0,0 +1,38 @@
+import api from '@/api'
+
+const reminderStore = {
+  namespaced: true,
+  state: {
+    myReminders: []
+  },
+  mutations: {
+    SET_REMINDERS (state, arr) {
+      state.myReminders = arr
+    }
+  },
+  actions: {
+    async refreshReminders ({dispatch}) {
+      console.log('refresh')
+      const books = await api.getReminders()
+      dispatch('set_books', books)
+    },
+    set_reminders ({commit}, arr) {
+      console.log(arr)
+      commit('SET_REMINDERS', arr)
+    },
+    async saveReminder ({dispatch}, reminder) {
+      await api.createReminder(reminder)
+      await dispatch('refreshReminders')
+    },
+    async deleteReminder ({dispatch}, id) {
+      await api.deleteReminder(id)
+      await dispatch('refreshReminders')
+    },
+    async updateReminder ({dispatch}, [id, obj]) {
+      await api.updateReminder(id, obj)
+      await dispatch('refreshReminders')
+    }
+  }
+}
+
+export default reminderStore
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import bookStore from './book/store_book'
+import reminderStore from './reminder/store_reminders'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    book: bookStore
+    book: bookStore,
+    reminder: reminderStore
   }
 })
