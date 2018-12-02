74e7410b87917249213809332ca56bd9a1239b87
unknown <judit.pacsai@gmail.com>
2018-12-02 23:24:53 +0100

    feat: sort reminders by date

--- src/store/reminder/helpers/addDate.js
+++ src/store/reminder/helpers/addDate.js
@@ -2,5 +2,6 @@ export default function addDate (arr) {
   const parsed = arr.map(r => Object.assign({
     dateObj: new Date(Date.UTC(r.year, r.month, r.day, 0, 0, 0, 0))
   }, r))
+  parsed.sort((a, b) => (a.dateObj).getTime() - (b.dateObj).getTime())
   return parsed
 }
