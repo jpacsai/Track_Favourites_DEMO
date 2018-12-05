d14848df307ba0acd521c7b2f564e6f97112c5c9
unknown <judit.pacsai@gmail.com>
2018-12-05 15:27:24 +0100

    chore: add comment to booklist component html section

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,14 +1,21 @@
 <template>
     <li :class="{ future: future }" class="book_li">
       <p class="order-num">{{ this.num }}</p>
+      <!-- BOOK THUMBNAIL IMAGE -->
       <div class="img-container">
         <a :href='sourceUrl' target='_blank'><img :src="image"/></a>
       </div>
+      <!-- / -  -  -  -  - / - INFO SECTION - / -  -  -  -  - / -->
       <div class='info'>
+        <!-- TITLE -->
         <p class='title'>{{ this.title }}</p>
+        <!-- AUTHOR -->
         <p v-if="view === 'search' || section === 'library'" class="author">by <span class='author-link' @click='authorBooks'>{{ this.author }}</span> - {{ this.year }}</p>
+        <!-- RELEASE YEAR -->
         <p v-if="view !== 'search'" class="author">{{ this.year }}</p>
+        <!-- / -  -  -  -  - / - RATINGS - / -  -  -  -  - / -->
         <div class="ratings" >
+          <!-- STAR RATING -->
           <svg width="100px" height="25px" viewBox="0 0 100 25" >
             <path d="M9.29 1.1 L3.3 21.78 L19.8 8.58 L0 8.58 L16.5 21.78
                      M28.29 1.1 L22.3 21.78 L38.8 8.58 L19 8.58 L35.5 21.78
@@ -23,27 +30,39 @@
               </linearGradient>
             </defs>
           </svg>
+          <!-- NUMBER RATING -->
           <p>{{ this.rating }} avg rating</p>
         </div> 
       </div>
+      <!-- / -  -  -  -  - / - DETAIL SECTION - / -  -  -  -  - / -->
       <div class="details">
+        <!-- SERIES BUTTON -->
         <button v-if="this.section === 'library' || this.series === true && view !== 'series'" class="series-btn" @click='searchSeries'> Series</button>
+        <!-- BOOK'S POSITON IN SERIES -->
         <p v-if="view === 'series' && section === 'explorer' && this.position" class="serie-position">BOOK <span>{{ this.position }}</span></p>
+        <!-- FUTURE RELEASE DATE -->
         <p class="future-release" v-if='this.future === true'>Coming on {{ this.release }}</p>
       </div>
+      <!-- / -  -  -  -  - / - LIBRARY DETAILS SETTINGS - / -  -  -  -  - / -->
       <div v-if='this.liked === true || this.section === "library"' class="shelf-container" >
+        <!-- SHELF SETTINGS -->
         <select :value="this.shelf" @change="selectChange">
           <option value="want">Want to read</option>
           <option value="reading">Currently reading</option>
           <option value="read">Read</option>
         </select>
+        <!-- OWNERSHIP CHECKBOX -->
         <input type="checkbox" name="ownership" :checked="this.owned" @change="ownerChange"> I have it<br>
       </div> 
+      <!-- / -  -  -  -  - / - LIBRARY REMOVE ICON - / -  -  -  -  - / -->
       <div v-if='this.section === "library"' class="heart-container">
         <img class='heart' v-bind:src="removeIcon" @mouseover="removeIconToggle" @mouseout="removeIconToggle" alt="remove icon" @click='removeBook'/>
       </div>
+      <!-- / -  -  -  -  - / - LIKE ICON - / -  -  -  -  - / -->
       <div v-if='this.section !== "library"' class="heart-container">
+        <!-- NOT LIKED -->
         <img class='heart' v-if='this.liked === false' src="../assets/heart_empty.svg" alt="heart" @click='likeToggle'>
+        <!-- LIKED -->
         <img class='heart' v-if='this.liked === true' src="../assets/heart_red.svg" alt="heart" @click='likeToggle'>
       </div>
     </li>
