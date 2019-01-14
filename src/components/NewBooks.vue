f7afc7d427803d5f0e3cd768df9e2b6fdacd14d0
unknown <judit.pacsai@gmail.com>
2019-01-14 18:53:02 +0100

    refactor: remove loading message

--- src/components/NewBooks.vue
+++ src/components/NewBooks.vue
@@ -5,20 +5,20 @@
         <input type="text" v-model='newSearch' required class="book_search-input" />
         <input type='submit' value="Search" class="book_search-submit" />
       </form>
-      <b-alert :show="loading" variant="info" class="loading_indicator">Loading...</b-alert>
-      <p class="book_searchNums" v-if="this.allResults > 0 && loading === false">
+      <!-- <b-alert :show="loading" variant="info" class="loading_indicator">Loading...</b-alert> -->
+      <p class="book_searchNums" v-if="this.allResults > 0 /* && loading === false */ ">  
         <!--
         <span>results from {{ this.resultsFrom}} to {{ this.resultsTo }} out of {{ this.allResults }}</span> -->
-        <span>total results {{ this.allResults }}</span>
-      </p>
+        <span>total result{{ this.allResults > 1 ? 's' : ''}} {{ this.allResults }} book{{ this.allResults > 1 ? 's' : ''}}</span>
+      </p> 
     </div>
-    <search v-if="view === 'search' && loading === false && displayList.length > 0" />
-    <series v-if="view === 'series' && loading === false" />
-    <author v-if="view === 'author' && loading === false" />
+    <search v-if="view === 'search' /* && loading === false */ && displayList.length > 0" />
+    <series v-if="view === 'series' /* && loading === false */ " />
+    <author v-if="view === 'author' /* && loading === false */ " />
 
     <div v-if="view === 'error'" class="error"><p>{{ this.error }}</p></div>
 
-    <pagenumbers v-if="displayList.length > 0 && view !== 'series' && loading === false" />
+    <!-- <pagenumbers v-if="displayList.length > 0 && view !== 'series' && loading === false" /> -->
 
   </div>
 </template>
@@ -27,7 +27,7 @@
 import series from './Series'
 import search from './Search'
 import author from './Author'
-import pagenumbers from './PageNums'
+// import pagenumbers from './PageNums'
 import { mapState, mapActions } from 'vuex'
 
 export default {
@@ -35,15 +35,15 @@ export default {
   components: {
     series,
     search,
-    author,
-    pagenumbers
+    author /* ,
+    pagenumbers */
   },
   computed: {
     ...mapState('book/library', {
       library: state => state.myBooks
     }),
     ...mapState('book/newBooks', {
-      loading: state => state.loading,
+      // loading: state => state.loading,
       view: state => state.view,
       error: state => state.error,
       displayList: state => state.displayList, /*
